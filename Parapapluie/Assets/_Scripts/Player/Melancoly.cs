b2677a18e3942ec79541e6b2fc890547dd027692
Chalivat <Chalivat.quentin@gmail.com>
2019-11-21 18:55:33 +0100

    Enkulos

--- Parapapluie/Assets/_Scripts/Player/Melancoly.cs
+++ Parapapluie/Assets/_Scripts/Player/Melancoly.cs
@@ -7,21 +7,16 @@ public class Melancoly : MonoBehaviour
     private float melancoly;
     public float rate;
     public bool isInPluie;
-    public bool isInCascade;
     public bool isMelancoly;
-
-    private int CascadeCount;
     void Start()
     {
-        particles = new List<GameObject>();
+        
     }
     
     void Update()
     {
-        Debug.Log(" Gros CON : " + melancoly);
-        Cascading();
         Melancoling();
-        
+
         if (melancoly > 0.9f)
         {
             isMelancoly = true;
@@ -40,17 +35,6 @@ public class Melancoly : MonoBehaviour
 
         melancoly = Mathf.Clamp01(melancoly);
     }
-
-
-    void Cascading()
-    {
-        if (CascadeCount > 0)
-        {
-            melancoly = 1;
-            isInCascade = true;
-        }
-        else isInCascade = false;
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Pluie"))
@@ -66,33 +50,7 @@ public class Melancoly : MonoBehaviour
         }
     }
 
-    void OnParticleCollision(GameObject other)
-    {
-        Debug.Log("Ouille");
-        if (other.gameObject.CompareTag("Cascade"))
-        {
-            CascadeCount++;
-        }
-    }
-
-    private List<GameObject> particles;
-    void Sort(GameObject other)
-    {
-            for (int i = 0; i < particles.Count; i++)
-            {
-                if (particles[i] == null)
-                {
-                    CascadeCount--;
-                    particles.Remove(particles[i]);
-                }
-            
-            }
-        }
-    }
 
 
 
-
-
-
-
+}
