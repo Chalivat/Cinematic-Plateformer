95e3c40eacb9155700f0ca25a1bca647e7844ef0
Bryndye <57522753+Bryndye@users.noreply.github.com>
2019-11-21 15:20:01 +0100

    Level 2 + event

---
+++ Parapapluie/Assets/Event_t2_murBreak.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_t2_murBreak : MonoBehaviour
+{
+    public GameObject Mur_break;
+    public GameObject Particles_pluie;
+    public GameObject Particles_explosion;
+
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Mur_break.SetActive(false);
+            Particles_pluie.SetActive(true);
+            Particles_explosion.SetActive(true);
+        }
+    }
+}
